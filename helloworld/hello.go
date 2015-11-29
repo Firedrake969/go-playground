89b3c5f5b9a8c9afc48710d07682010ff55b76d6
Firedrake969 <firedrake969@users.noreply.github.com>
2015-11-28 20:04:14 -0700

    cooler stuff, yay

--- helloworld/hello.go
+++ helloworld/hello.go
@@ -5,5 +5,15 @@ import (
 )
 
 func main() {
-    fmt.Println("Hello, cliched world!")
+    hello := "Hello, cliched world!"
+    fmt.Printf("%s\n", hello) // must use printf to interpolate?
+    fmt.Printf("Variable hello is a %T.\n", hello)
+    fmt.Println("Counting down from 10.")
+    for i := 10; i > 0; i-- {
+        if i != 1 {
+            fmt.Printf("%v... ", i)
+        } else {
+            fmt.Printf("%v!", i)
+        }
+    }
 }
