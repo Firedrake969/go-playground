1827c0fd3fdb36ea15a88f37dfeff3ca830c9b7f
Firedrake969 <firedrake969@users.noreply.github.com>
2015-11-28 20:23:42 -0700

    pointers, slices

---
+++ pointyarrays/pointyarrays.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+    "fmt"
+)
+
+// underscores or camelcase?
+
+func main() {
+    fmt.Println("Pointers!")
+
+    somenumber := 9000
+    pointynumber := &somenumber
+    fmt.Printf("somenumber's literal value is %v.  pointynumber's literal value is %v.\n", somenumber, pointynumber)
+    fmt.Printf("pointynumber's \"through\" value is %v.\n", *pointynumber)
+    *pointynumber += 1
+    fmt.Printf("the numbers' new values are %v and %v.", somenumber, *pointynumber)
+
+    fmt.Println("")
+
+    fmt.Println("Slices!")
+    intarr := []int{} // slices vs arrays?  I think I like slices
+    intarr = append(intarr, 1337) // kind of annoying... not a function of a slice?
+    fmt.Println(intarr)
+
+    for i := 0; i < 500; i++ {
+        intarr = append(intarr, i * 50)
+    }
+
+    fmt.Printf("intarr is %v integers long.  It should be 501 integers long.\n", len(intarr))
+
+    fmt.Println("")
+}
